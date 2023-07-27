711f02feeec405ba395786a2b8ad848419008ccc
Benjamin Linford <blinford@live.unc.edu>
2023-07-27 20:54:44 +0100

    minimum code to pass three tests

--- src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
+++ src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
@@ -3,6 +3,11 @@ package com.tdd.findhighestnumber;
 public class HighestNumberFinder {
 
     public int findHighestNumber(int[] arr) {
+        if(arr.length == 1)
+            return arr[0];
+        else if(arr.length == 2)
+            return arr[0] < arr[1] ? arr[1] : arr[0];
 
+        return 0;
     }
 }
